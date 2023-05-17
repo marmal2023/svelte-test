1fb1c3408fe3e6af6f1398070025dd82de88be7b
Marina <dialogmalysheva@yandex.ru>
2023-05-17 13:56:45 +0300

    Скрыли менюшку

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,40 @@
-
+<script>
+    const nav = [
+        {
+            title: "Обо мне",
+            href: "/about",
+            isShow: true,
+        },
+        {
+            title: "Блог",
+            href: "/blog",
+            isShow: true,
+        },
+        {
+            title: "Портфолио",
+            href: "/project",
+            isShow: true,
+        },
+        {
+            title: "Контакты",
+            href: "/contact",
+            isShow: true,
+        },
+        {
+            title: "temp",
+            href: "/temp",
+            isShow: false,
+        },
+    ];
+</script>
 
 <nav>
     <div class="nav-container">
-        <a href="/" class="nav-logo" title="Back to Homepage"
-            >Имя Фамилия</a
-        >
+        <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
         <div class="nav-links">
-            {#each nav as link}
+            {#if link.isShow}
                 <a href={link.href} class="link">{link.title}</a>
-            {/each}
+            {/if}
         </div>
     </div>
 </nav>
@@ -17,30 +43,7 @@
     <!-- Pages will be injected below -->
     <slot />
 </div>
-<script>
-    const nav = [
-        {
-            title: "Обо мне",
-            href: "/about",
-        },
-        {
-            title: "Блог",
-            href: "/blog",
-        },
-        {
-            title: "Портфолио",
-            href: "/project",
-        },
-        {
-            title: "Контакты",
-            href: "/contact",
-        },
-        {
-            title: "temp",
-            href: "/temp",
-        }
-    ];
-</script>
+
 <style>
     .container {
         max-width: 1400px;
